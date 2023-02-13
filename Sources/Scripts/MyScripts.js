1829c0a7c31b0b2c7e459bf181ffb036c9b84bfb
Nicode <victornicorios@gmail.com>
2023-02-13 13:49:10 -0300

    CodeHome 0.5: Autodescription content

--- Sources/Scripts/MyScripts.js
+++ Sources/Scripts/MyScripts.js
@@ -2,19 +2,27 @@ let slideIndex = 1;
 showSlides(slideIndex);
 
 function showSlides(n) {
+  //Top Image controller
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
+  let descripion = document.getElementsByClassName("textbodybox");
+
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+    descripion[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
+  descripion[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+
+  // Description activator
+
     }
   // Next/previous controls
 function plusSlides(n) {
